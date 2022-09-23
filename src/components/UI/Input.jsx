60f57a80ddc412b50485a2875120ebf73b1033e9
J.D. Browne <PeshwariNaan01@proton.me>
2022-09-23 10:56:25 +0400

    added cartItem component. Added logic to cartProvider to add item to cart and increase the amount. Add logic to mealItem to add item.

--- src/components/UI/Input.jsx
+++ src/components/UI/Input.jsx
@@ -1,12 +1,15 @@
+import React from 'react';
 import classes from './Input.module.css';
 
-const Input = (props) => {
+
+//I want to use refs in a custom component - I need to wrap the custom component in React.forwardRef
+const Input = React.forwardRef((props, ref) => {
   return (
     <div className={classes.input}>
       <label htmlFor={props.input.id}>{props.label}</label>
-      <input {...props.input} />
+      <input ref={ref} {...props.input} />
     </div>
   );
-};
+});
 
 export default Input;
--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,16 +1,59 @@
+import { useReducer } from 'react';
 import CartContext from './cart.context';
 
-const CartProvider = (props) => {
-    const addItemHandler = (item) => { }
-    const removeItemHandler = (id) => {}
-    const cartContext = {
-        items: [],
-        totalAmount: 0,
-        addItem: addItemHandler,
-        removeItem: removeItemHandler,
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
 
-    }
-  return <CartContext.Provider value={cartContext}>{props.children}</CartContext.Provider>;
+const cartReducer = (state, action) => {
+  if (action.type === 'ADD_CART_ITEM') {    
+
+    const existingCartItemIndex = state.items.findIndex(item => item.id === action.item.id)
+    const existingCartItem = state.items[existingCartItemIndex]   
+    let updatedItems
+
+    if(existingCartItem) {      
+      const updatedItem = {
+        ...existingCartItem, amount: existingCartItem.amount + action.item.amount
+      }
+      updatedItems = [...state.items]
+      updatedItems[existingCartItemIndex] = updatedItem
+    }else {      
+      updatedItems = state.items.concat(action.item);
+    }    
+
+    const updatedTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount;
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
+  }
+};
+
+const CartProvider = (props) => {
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
+  const addItemHandler = (item) => {
+    dispatchCartAction({ type: 'ADD_CART_ITEM', item: item });
+  };
+  const removeItemHandler = (id) => {
+    dispatchCartAction({ type: 'REMOVE_CART_ITEM', id: id });
+  };
+  const cartContext = {
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
+    addItem: addItemHandler,
+    removeItem: removeItemHandler,
+  };
+  return (
+    <CartContext.Provider value={cartContext}>
+      {props.children}
+    </CartContext.Provider>
+  );
 };
 
 export default CartProvider;
